079d34d8bc8520db30ad0465b09f9259a836ab84
kezunhao <kezunhao@yeah.net>
2020-09-11 12:15:58 +0800

    etcd 启动shell

--- service/upload/config/config.go
+++ service/upload/config/config.go
@@ -1,7 +1,7 @@
 package config
 
 // UploadEntry : 配置上传入口地址
-var UploadEntry = "localhost:28080"
+var UploadEntry = "upload.fileserver.com"
 
 // UploadServiceHost : 上传服务监听的地址
 var UploadServiceHost = "0.0.0.0:28080"
